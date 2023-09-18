0a0f9d5c67685a1932625652c897db7c934871c1
Tobias König <tobiaskoenig1@gmail.com>
2023-09-18 17:51:17 +0200

    handle issues parsing images
    - no hard failure on cfg, steps or seed

--- nodes.py
+++ nodes.py
@@ -7,6 +7,8 @@ from .sd_prompt_reader.image_data_reader import ImageDataReader
 
 from PIL import Image, ImageOps
 
+debug = False
+
 class LoadImageWithMetadata:
     @classmethod
     def INPUT_TYPES(s):
@@ -23,6 +25,12 @@ class LoadImageWithMetadata:
     FUNCTION = "load_image"
 
     def load_image(self, image):
+        image, mask, image_path = self.read_image(image)        
+        reader = self.parse_metadata(image_path)
+
+        return self.parse_result(reader, image, mask)
+
+    def read_image(self, image):
         image_path = folder_paths.get_annotated_filepath(image)
         i = Image.open(image_path)
         i = ImageOps.exif_transpose(i)
@@ -34,19 +42,53 @@ class LoadImageWithMetadata:
             mask = 1. - torch.from_numpy(mask)
         else:
             mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
-        
+        return image, mask, image_path
+
+    def parse_metadata(self, image_path):
         reader = ImageDataReader(image_path)
-        #reader.parameter: {'model': 'abc', 'sampler': 'DPM++ 2M Karras', 'seed': '2706265200', 'cfg': '7', 'steps': '25', 'size': '512x512'}
         if(reader.tool == ''):
             raise ValueError('Unable to read generation metadata from image!')
+        
+        return reader
+    
+    def parse_result(self, reader: ImageDataReader, image: Image, mask):
+        try:        
+            seed = int(reader.parameter["seed"])
+        except:
+            print("Failed to parse seed '{0}'".format(reader.parameter["seed"]))
+            seed = 0
 
-        seed = int(reader.parameter["seed"])
-        cfg = float(reader.parameter["cfg"])
-        steps = int(reader.parameter["steps"])
-        size = reader.parameter["size"]
-        sizeSplit = size.split("x")
-        width = int(sizeSplit[0])
-        height = int(sizeSplit[1])
+        try:
+            size = reader.parameter["size"]
+            sizeSplit = size.split("x")
+            width = int(sizeSplit[0])
+            height = int(sizeSplit[1])
+        except:
+            print("Failed to parse size '{0}'".format(reader.parameter["size"]))
+            width = 0
+            height = 0
+
+        try:
+            cfg = float(reader.parameter["cfg"])
+        except:
+            print("Failed to parse cfg '{0}'".format(reader.parameter["cfg"]))
+            cfg = 0.0     
+        
+        try:
+            steps = int(reader.parameter["steps"])
+        except:
+            print("Failed to parse steps '{0}'".format(reader.parameter["steps"]))
+            steps = 0
+
+        if debug:
+            print("Read metadata from image")
+            print("Tool: {0}".format(reader.tool))
+            print("Positive: {0}".format(reader.positive))
+            print("Negative: {0}".format(reader.negative))
+            print("Size: {0}x{1}".format(width, height))
+            print("Seed: {0}".format(seed))
+            print("Cfg: {0}".format(cfg))
+            print("Steps: {0}".format(steps))
         
         return (image, mask, reader.positive, reader.negative, seed, width, height, cfg, steps)
 
